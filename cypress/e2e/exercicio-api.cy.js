c3a3636e22865270a7a4f550b07a078782c30d51
jessepr372 <105854432+jessepr372@users.noreply.github.com>
2024-12-11 19:01:10 -0300

    Primeiro commit do projeto de automação API

--- cypress/e2e/exercicio-api.cy.js
+++ cypress/e2e/exercicio-api.cy.js
@@ -2,29 +2,115 @@
 
 describe('Testes da Funcionalidade Usuários', () => {
 
+  let token;
+  const baseUrl = '/usuarios';
+
+  before(() => {
+    token = Cypress.env('auth_token');
+  });
+
+  const requestApi = (method, url, body = null, failOnStatusCode = true) => {
+    return cy.request({
+      method,
+      url,
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+      body,
+      failOnStatusCode
+    });
+  };
+
   it('Deve validar contrato de usuários', () => {
-    //TODO: 
+    requestApi('GET', baseUrl).then((response) => {
+      expect(response.status).to.equal(200);
+      const usuarios = response.body.usuarios;
+      expect(usuarios).to.be.an('array');
+      expect(usuarios[0]).to.have.all.keys('_id', 'nome', 'email', 'password', 'administrador');
+      expect(usuarios[0]._id).to.be.a('string');
+      expect(usuarios[0].nome).to.be.a('string');
+      expect(usuarios[0].email).to.be.a('string');
+      expect(usuarios[0].password).to.be.a('string');
+      expect(usuarios[0].administrador).to.be.a('string');
+    });
   });
 
   it('Deve listar usuários cadastrados', () => {
-    //TODO: 
+    requestApi('GET', baseUrl).then((response) => {
+      expect(response.status).to.equal(200);
+      const usuarios = response.body.usuarios;
+      expect(usuarios).to.be.an('array');
+      expect(usuarios.length).to.be.greaterThan(0);
+      expect(usuarios[0]).to.have.all.keys('_id', 'nome', 'email', 'password', 'administrador');
+    });
   });
 
   it('Deve cadastrar um usuário com sucesso', () => {
-    //TODO: 
+    const uniqueEmail = `novoUsuario_${Date.now()}@qa.com`;
+
+    requestApi('POST', baseUrl, {
+      nome: "Novo Usuário",
+      email: uniqueEmail,
+      password: "senha123",
+      administrador: "false"
+    }).then((response) => {
+      expect(response.status).to.equal(201);
+      expect(response.body.message).to.equal('Cadastro realizado com sucesso');
+      expect(response.body).to.have.property('_id');
+    });
   });
 
   it('Deve validar um usuário com email inválido', () => {
-    //TODO: 
+    const emailInvalido = "usuario@invalid.com";
+    
+    requestApi('POST', baseUrl, {
+      nome: "Usuário com E-mail Inválido",
+      email: emailInvalido,
+      password: "senha123",
+      administrador: "false"
+    }, false).then((response) => {
+      expect(response.status).to.equal(400);
+      expect(response.body.message).to.equal('Este email já está sendo usado');
+    });
   });
 
   it('Deve editar um usuário previamente cadastrado', () => {
-    //TODO: 
+    const usuarioId = '0uxuPY0cbmQhpEz1'; 
+    const novoNome = 'Usuário Editado';
+    const novoEmail = 'usuarioeditar2@teste.com';
+
+    requestApi('PUT', `${baseUrl}/${usuarioId}`, {
+      nome: novoNome,
+      email: novoEmail,
+      password: "senha123",
+      administrador: "false"
+    }).then((response) => {
+      expect(response.status).to.equal(200);
+      expect(response.body.message).to.equal('Registro alterado com sucesso');
+    });
+
+    requestApi('GET', `${baseUrl}/${usuarioId}`).then((response) => {
+      expect(response.body.nome).to.equal(novoNome);
+      expect(response.body.email).to.equal(novoEmail);
+    });
   });
 
   it('Deve deletar um usuário previamente cadastrado', () => {
-    //TODO: 
-  });
+    const novoUsuario = {
+      nome: 'Novo Usuário',
+      email: 'novousuario@teste.com',
+      password: 'senha123',
+      administrador: 'false'
+    };
 
+    requestApi('POST', baseUrl, novoUsuario).then((response) => {
+      const usuarioId = response.body._id;
+
+      requestApi('DELETE', `${baseUrl}/${usuarioId}`).then((deleteResponse) => {
+        expect(deleteResponse.status).to.equal(200);
+        expect(deleteResponse.body.message).to.equal('Registro excluído com sucesso');
+      });
+    });
+  });
 
 });
--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,20 +1,40 @@
 /// <reference types="cypress" />
 
 describe('Login', () => {
-
-    it('Deve fazer login com sucesso', () => {
-        cy.request({
-            method: 'POST',
-            url: 'login',
-            body: {
-                "email": "fulano@qa.com",
-                "password": "teste" 
-            }
-        }).then((response) => {
-            expect(response.status).to.equal(200)
-            expect(response.body.message).to.equal('Login realizado com sucesso')
-            cy.log(response.body.authorization)
-        })
+    let token;
+  
+    before(() => {
+      cy.request({
+        method: 'POST',
+        url: 'login', // Ajuste conforme a URL da sua API
+        body: {
+          email: "fulano@qa.com", // Substitua pelo email de um usuário válido
+          password: "teste" // Substitua pela senha válida
+        }
+      }).then((response) => {
+        token = response.body.authorization; // Armazenando o token
+      });
     });
-
-});
+  
+    it('Deve fazer login com sucesso', () => {
+      cy.request({
+        method: 'POST',
+        url: 'login',
+        body: {
+          "email": "fulano@qa.com",
+          "password": "teste"
+        }
+      }).then((response) => {
+        expect(response.status).to.equal(200);
+        expect(response.body.message).to.equal('Login realizado com sucesso');
+        cy.log(response.body.authorization);
+      });
+    });
+  
+    // Podemos exportar o token para ser utilizado nos outros testes
+    after(() => {
+      // Exporta o token globalmente
+      Cypress.env('auth_token', token);
+    });
+  });
+  
